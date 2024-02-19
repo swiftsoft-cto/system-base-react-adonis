909f053fcddd168fd9fab2423fc7e5b4c76c276a
Higor Soares <codehs07@gmail.com>
2024-02-19 07:59:41 -0300

    Private Route

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,19 @@
+//src/App.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import RecoverPass from "./pages/RecoverPass";
+import PrivateRoute from "./components/PrivateRoute"; // Importe o componente PrivateRoute
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route element={<PrivateRoute />}> // Envolve as rotas protegidas com PrivateRoute
+          <Route path="/home" element={<Home />} />
+        </Route>
         <Route path="/redefinir-senha/:token" element={<RecoverPass />} />
       </Routes>
     </BrowserRouter>
