957d40dd5a291e937f028830e3453671749fea09
Higor Soares <codehs07@gmail.com>
2024-02-24 13:38:19 -0300

    Loader MUI

--- web/src/utils/PrivateRoute/index.tsx
+++ web/src/utils/PrivateRoute/index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 interface PrivateRouteProps {
   accessLevel?: number;
@@ -11,7 +13,18 @@ const PrivateRoute = ({ accessLevel }: PrivateRouteProps) => {
   const { isAuthenticated, loading, userAccessLevel }:any = useAuth();
 
   if (loading) {
-    return <div>Carregando...</div>; 
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh', 
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
   }
 
   if (!isAuthenticated) {
